bea24df56b8d34c47d8c57b434323660827f4d79
julianopita <50183295+julianopita@users.noreply.github.com>
2020-03-11 19:11:04 -0300

    modal

--- js/modal.js
+++ js/modal.js
@@ -1,22 +1,32 @@
-const toggleModal = () => {
-  document.querySelector('.box_image')
-    .classList.toggle('box_image--hidden');
-  document.querySelector('.overlay')
-    .classList.toggle('overlay--hidden');
-}
-document.querySelector('#show-box_image')
-  .addEventListener('click', toggleModal);
+var b = modal_id();
 
-document.querySelector('.overlay')
-  .addEventListener('click', toggleModal);
+function modal_id(obj)
+{
+  var a = obj.id;
+  console.log (b);
+};
 
-document.querySelector('#learn-more-form')
-  .addEventListener('submit', (e) => {
-  e.preventDefault();
-  toggleModal();
-});
+  var btn =  document.getElementById(b);
 
-document.querySelector('.box_image__close-bar span')
-  .addEventListener('click', toggleModal);
-document.querySelector('.overlay')
-  .addEventListener('click', toggleModal);
+  // Get the button that opens the modal
+  var modal = document.getElementById("cont_" + b);
+
+  // Get the <span> element that closes the modal
+  var span = document.getElementsByClassName("close_" + b)[0];
+
+    // When the user clicks on the button, open the modal
+    btn.onclick = function() {
+      modal.style.display = "block";
+    }
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+      modal.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+      if (event.target == modal) {
+        modal.style.display = "none";
+      }
+  }
