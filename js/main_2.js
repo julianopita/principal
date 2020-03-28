103dc59122246391cd11a2b330d9d9e009fe5547
Victor Silva <victorsilva698@usp.br>
2020-03-27 23:34:00 -0300

    atualização da timeline, porém com um bug nas relevancias

--- js/main_2.js
+++ js/main_2.js
@@ -26,7 +26,7 @@ function ano(text){
         var texto = document.createTextNode(anos[i]);
         var divNova = document.createElement('div');
         divNova.appendChild(texto);
-        divNova.setAttribute('id','mes');
+        divNova.setAttribute('id','ano');
         timeline.appendChild(divNova);
         mes(text,i,anos);
     }
@@ -34,7 +34,7 @@ function ano(text){
 //adiciona os meses de forma dinamica
 function mes(text,i,anos){
     var meses = Object.keys(text[anos[i]]);
-    alert(meses);
+    // alert(meses);
     for(let j =0;j< meses.length;j++){
         //Cria nó e div, adiciona nó a div e seta atributos
         //depois adiciona a timeline
@@ -49,15 +49,114 @@ function mes(text,i,anos){
     }
 }
 function dia(text,i,j,anos,meses){
+    
     var dias = Object.keys(text[anos[i]][meses[j]]);
-    alert(dias)
-    for(let j = 0;j < dias.length;j++)
+    var rel = Object.values(text[anos[0]][meses[j]]);
+    for(let k = 0;k < dias.length;k++)
     {
+        relevancia = rel[k];
         var div_bloco = document.createElement('div');
         div_bloco.setAttribute('id','bloco');   
         //texto_2 é o dia do evento
         var data = document.createElement('div');
         timeline.appendChild(div_bloco);
+        if (relevancia == 0) {
+            // // data
+            data.setAttribute('id','data');
+            data.setAttribute('class','data p0');
+            var texto_2 = document.createTextNode(dias[k]);
+            data.appendChild(texto_2);
+            div_bloco.appendChild(data);
+        } else if (relevancia == 1) {
+            // // data
+            data.setAttribute('id','data');
+            data.setAttribute('class','data p1');
+            var texto_2 = document.createTextNode(dias[k]);
+            data.appendChild(texto_2);
+            div_bloco.appendChild(data);
+        } else {
+            // // data
+            data.setAttribute('id','data');
+            data.setAttribute('class','data p2');
+            var texto_2 = document.createTextNode(dias[k]);
+            data.appendChild(texto_2);
+            div_bloco.appendChild(data);
+        };
+                    
+        var linha_horizontal = document.createElement('div');
+        linha_horizontal.setAttribute('id','linha_horizontal');
+        div_bloco.appendChild(linha_horizontal)
+
+        
+        var bloco_direita = document.createElement('div');
+        bloco_direita.setAttribute('id','bloco_direita')
+        var evento = document.createElement('div');
+        evento.setAttribute('id','evento');
+
+        if (relevancia == 0) {
+            
+            // imagem por relevância
+            var img = document.createElement('img');
+            img.setAttribute('id','imagem');
+            img.src = "./imagens/template" + relevancia + ".jpg";
+            img.width = 50;
+            img.height = 50;
+
+            // texto_3
+            texto_3 = document.createTextNode(Object.values(rel[0]));
+            evento.appendChild(texto_3);
+            bloco_direita.appendChild(img);
+            bloco_direita.setAttribute('class','bloco_direita');
+            bloco_direita.appendChild(evento);
+            div_bloco.appendChild(bloco_direita);
+
+        } else if (relevancia == 1) {
+
+            // imagem por relevância
+            var img = document.createElement('img');
+            img.setAttribute('id','imagem');
+            img.src = "./imagens/template" + relevancia + ".jpg";
+            img.width = 50;
+            img.height = 50;
+
+            // // texto_3
+            texto_3 = document.createTextNode(Object.values(rel[0]));
+            evento.appendChild(texto_3);
+            bloco_direita.appendChild(img);
+            bloco_direita.setAttribute('class','bloco_direita');
+            bloco_direita.appendChild(evento);
+            div_bloco.appendChild(bloco_direita);
+        } else {
+
+            // imagem por relevância
+            var img = document.createElement('img');
+            img.setAttribute('id','imagem');
+            img.src = "./imagens/template" + relevancia + ".jpg";
+            img.width = 50;
+            img.height = 50;
+
+            // // texto_3
+            //essa parte eu n entendi
+            texto_3 = document.createTextNode(Object.values(rel[0]));
+            evento.appendChild(texto_3);
+            bloco_direita.appendChild(img);
+            bloco_direita.setAttribute('class','bloco_direita');
+            bloco_direita.appendChild(evento);
+            div_bloco.appendChild(bloco_direita);
+        };
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
 
@@ -66,90 +165,90 @@ function dia(text,i,j,anos,meses){
   
             
 //             //relevancia
-//             relevancia = Object.keys(text[meses[i]][dias[j]]);
-//             if (relevancia == 0) {
-//                 // // data
-//                 data.setAttribute('id','data');
-//                 data.setAttribute('class','data p0');
-//                 var texto_2 = document.createTextNode(dias[j]);
-//                 data.appendChild(texto_2);
-//                 div_bloco.appendChild(data);
-//             } else if (relevancia == 1) {
-//                 // // data
-//                 data.setAttribute('id','data');
-//                 data.setAttribute('class','data p1');
-//                 var texto_2 = document.createTextNode(dias[j]);
-//                 data.appendChild(texto_2);
-//                 div_bloco.appendChild(data);
-//             } else {
-//                 // // data
-//                 data.setAttribute('id','data');
-//                 data.setAttribute('class','data p2');
-//                 var texto_2 = document.createTextNode(dias[j]);
-//                 data.appendChild(texto_2);
-//                 div_bloco.appendChild(data);
-//             };
+            // relevancia = Object.keys(text[meses[i]][dias[j]]);
+            // if (relevancia == 0) {
+            //     // // data
+            //     data.setAttribute('id','data');
+            //     data.setAttribute('class','data p0');
+            //     var texto_2 = document.createTextNode(dias[j]);
+            //     data.appendChild(texto_2);
+            //     div_bloco.appendChild(data);
+            // } else if (relevancia == 1) {
+            //     // // data
+            //     data.setAttribute('id','data');
+            //     data.setAttribute('class','data p1');
+            //     var texto_2 = document.createTextNode(dias[j]);
+            //     data.appendChild(texto_2);
+            //     div_bloco.appendChild(data);
+            // } else {
+            //     // // data
+            //     data.setAttribute('id','data');
+            //     data.setAttribute('class','data p2');
+            //     var texto_2 = document.createTextNode(dias[j]);
+            //     data.appendChild(texto_2);
+            //     div_bloco.appendChild(data);
+            // };
                         
-//             var linha_horizontal = document.createElement('div');
-//             linha_horizontal.setAttribute('id','linha_horizontal');
-//             div_bloco.appendChild(linha_horizontal)
+            // var linha_horizontal = document.createElement('div');
+            // linha_horizontal.setAttribute('id','linha_horizontal');
+            // div_bloco.appendChild(linha_horizontal)
 
             
-//             var bloco_direita = document.createElement('div');
-//             bloco_direita.setAttribute('id','bloco_direita')
-//             var evento = document.createElement('div');
-//             evento.setAttribute('id','evento');
+            // var bloco_direita = document.createElement('div');
+            // bloco_direita.setAttribute('id','bloco_direita')
+            // var evento = document.createElement('div');
+            // evento.setAttribute('id','evento');
 
-//             if (relevancia == 0) {
+            // if (relevancia == 0) {
                 
-//                 // imagem por relevância
-//                 var img = document.createElement('img');
-//                 img.setAttribute('id','imagem');
-//                 img.src = "./imagens/template" + relevancia + ".jpg";
-//                 img.width = 50;
-//                 img.height = 50;
+            //     // imagem por relevância
+            //     var img = document.createElement('img');
+            //     img.setAttribute('id','imagem');
+            //     img.src = "./imagens/template" + relevancia + ".jpg";
+            //     img.width = 50;
+            //     img.height = 50;
 
-//                 // texto_3
-//                 texto_3 = document.createTextNode(text[meses[i]][dias[j]][0]);
-//                 evento.appendChild(texto_3);
-//                 bloco_direita.appendChild(img);
-//                 bloco_direita.setAttribute('class','bloco_direita');
-//                 bloco_direita.appendChild(evento);
-//                 div_bloco.appendChild(bloco_direita);
+            //     // texto_3
+            //     texto_3 = document.createTextNode(text[meses[i]][dias[j]][0]);
+            //     evento.appendChild(texto_3);
+            //     bloco_direita.appendChild(img);
+            //     bloco_direita.setAttribute('class','bloco_direita');
+            //     bloco_direita.appendChild(evento);
+            //     div_bloco.appendChild(bloco_direita);
 
-//             } else if (relevancia == 1) {
+            // } else if (relevancia == 1) {
 
-//                 // imagem por relevância
-//                 var img = document.createElement('img');
-//                 img.setAttribute('id','imagem');
-//                 img.src = "./imagens/template" + relevancia + ".jpg";
-//                 img.width = 50;
-//                 img.height = 50;
+            //     // imagem por relevância
+            //     var img = document.createElement('img');
+            //     img.setAttribute('id','imagem');
+            //     img.src = "./imagens/template" + relevancia + ".jpg";
+            //     img.width = 50;
+            //     img.height = 50;
 
-//                 // // texto_3
-//                 texto_3 = document.createTextNode(text[meses[i]][dias[j]][1]);
-//                 evento.appendChild(texto_3);
-//                 bloco_direita.appendChild(img);
-//                 bloco_direita.setAttribute('class','bloco_direita');
-//                 bloco_direita.appendChild(evento);
-//                 div_bloco.appendChild(bloco_direita);
-//             } else {
+            //     // // texto_3
+            //     texto_3 = document.createTextNode(text[meses[i]][dias[j]][1]);
+            //     evento.appendChild(texto_3);
+            //     bloco_direita.appendChild(img);
+            //     bloco_direita.setAttribute('class','bloco_direita');
+            //     bloco_direita.appendChild(evento);
+            //     div_bloco.appendChild(bloco_direita);
+            // } else {
 
-//                 // imagem por relevância
-//                 var img = document.createElement('img');
-//                 img.setAttribute('id','imagem');
-//                 img.src = "./imagens/template" + relevancia + ".jpg";
-//                 img.width = 50;
-//                 img.height = 50;
+            //     // imagem por relevância
+            //     var img = document.createElement('img');
+            //     img.setAttribute('id','imagem');
+            //     img.src = "./imagens/template" + relevancia + ".jpg";
+            //     img.width = 50;
+            //     img.height = 50;
 
-//                 // // texto_3
-//                 texto_3 = document.createTextNode(text[meses[i]][dias[j]][2]);
-//                 evento.appendChild(texto_3);
-//                 bloco_direita.appendChild(img);
-//                 bloco_direita.setAttribute('class','bloco_direita');
-//                 bloco_direita.appendChild(evento);
-//                 div_bloco.appendChild(bloco_direita);
-//             };
+            //     // // texto_3
+            //     texto_3 = document.createTextNode(text[meses[i]][dias[j]][2]);
+            //     evento.appendChild(texto_3);
+            //     bloco_direita.appendChild(img);
+            //     bloco_direita.setAttribute('class','bloco_direita');
+            //     bloco_direita.appendChild(evento);
+            //     div_bloco.appendChild(bloco_direita);
+            // };
         
 
 
